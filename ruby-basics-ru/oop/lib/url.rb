306554fc164757c33081691ee497415b8ae288ea
@hexlet/cli <support@hexlet.io>
2024-05-15 20:08:18 +0600

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -15,21 +15,22 @@ class Url
   def query_params
     result = {}
     params = url.split('?')[1]
-    params.split('&').each do |p|
+    params&.split('&')&.each do |p|
       key, value = p.split('=')
       result[key.to_sym] = value
     end
     result
   end
 
-  def query_param(key = nil, default)
-    puts self.query_params[key] || default
+  def query_param(key, default = nil)
+    self.query_params[key] || default
   end
 
   def ==(other)
     self.url == other.url
   end
-  def_delegators :attributed_url, :scheme, :host
+
+  def_delegators   :@attributed_url, :scheme, :host
 
   class Attribute
     attr_accessor :url
@@ -42,15 +43,11 @@ class Url
       url.split('://').first
     end
     def host
-      url.split('://').last.split('?').first
+      url.split('://').last.split('?').first.split(':').first
     end
   end
 end
 
 # END
-# url = Url.new 'https://yandex.ru?key=value&key2=value2'
-# host = url.host
-# scheme = url.scheme
-# puts host
-# puts scheme
-# puts url.query_param(:key2, 'default')
+url = Url.new 'https://google.com'
+puts url.query_param(:key2, 'lala')
